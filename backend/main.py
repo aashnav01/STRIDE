1e724b769b8ba43fd38e474d96483ae3806bf92a
gyanasritallati <gyanasri2007@gmail.com>
2026-08-26 17:23:01 +0530

    Allow Render frontend CORS origin

--- backend/main.py
+++ backend/main.py
@@ -61,8 +61,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://koa-frontend-o8um.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
